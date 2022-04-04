f9d90dd0b870b04e9e798beec7e22ea3f5ae3ae4
Tim Kent <timdekent@gmail.com>
2022-04-03 19:45:28 -0700

    game mode select

--- arcade/app.js
+++ arcade/app.js
@@ -9,84 +9,95 @@ const gameState = {
     ]
   };
 
-// Selectors
-
-const startGame = document.querySelector('#start-game');
-const table = document.querySelector('table');
-const tbody = document.getElementsByTagName('tbody');
+// Select Elements
+  // Sections
+const sectionSelectGameMode = document.querySelector('#select-game-mode');
+const sectionPlayerNames = document.querySelector('#section-player-names');
+const sectionPlayerHints = document.querySelector('#section-player-hints');
+const sectionStartGame = document.querySelector('#section-start-game');
+  // Forms
 const formPlayerOneName = document.querySelector('#form-player-one-name');
 const formPlayerTwoName = document.querySelector('#form-player-two-name');
-const inputPlayerOneName = document.querySelector('#player-one-name');
-const inputPlayerTwoName = document.querySelector('#player-two-name');
+const inputPlayerOneName = document.querySelector('#input-player-one-name');
+const inputPlayerTwoName = document.querySelector('#input-player-two-name');
+  // Buttons
+const buttonOnePlayerGame = document.querySelector('#button-one-player-game');
+const buttonTwoPlayerGame = document.querySelector('#button-two-player-game'); 
 
-// Player Names
 
+  // Game Board
+const table = document.querySelector('table');
+
+
+
+// Create Elements
+
+const createSpan = document.createElement('span');
+
+// Empty Variables & Arrays
+
+let onePlayerGame;
 let playerNames = [];
 
 // Event Listeners
+  // Select Game Mode
+
+buttonOnePlayerGame.addEventListener('click', function() {
+    onePlayerGame = true;
+    toggleStartingSections();
+});
+
+buttonTwoPlayerGame.addEventListener('click', function() {
+    onePlayerGame = false;
+    toggleStartingSections();
+});
 
   //Name Entry -- Player 1
-  
-  formPlayerOneName.addEventListener('submit', function(event) {
-      //const target = event.target;
-      //const parent = event.target.parentNode;
-      event.preventDefault();
-      playerNames.push(inputPlayerOneName.value);
-      formPlayerOneName.classList.toggle('hide');
-      formPlayerTwoName.classList.toggle('hide');
+
+formPlayerOneName.addEventListener('submit', function(event) {
+    event.preventDefault();
+    playerNames.push(inputPlayerOneName.value);
+    formPlayerOneName.classList.toggle('hide');
+    if (!onePlayerGame) {
+        formPlayerTwoName.classList.toggle('hide');
+    } 
+    else {
+        renderStartingBoard();
+    }
   });
 
   // Name Entry -- Player 2
   
-  formPlayerTwoName.addEventListener('submit', function(event) {
-      event.preventDefault();
-      playerNames.push(inputPlayerTwoName.value);
-      formPlayerTwoName.classList.toggle('hide');
-      console.log(playerNames);
-      renderStartingBoard();
+formPlayerTwoName.addEventListener('submit', function(event) {
+    event.preventDefault();
+    playerNames.push(inputPlayerTwoName.value);
+    formPlayerTwoName.classList.toggle('hide');
+    console.log(playerNames);
+    renderStartingBoard();
   });
 
 
+// Helper Functions
+  // Game Mode Select
 
+  let toggleStartingSections = () => {
+    sectionSelectGameMode.classList.toggle('hide');
+    sectionPlayerNames.classList.toggle('hide');
+};  
 
-
-function start(event) {
-    const target = event.target;
-    const parent = event.target.parentNode;
-    if (target.tagName === 'BUTTON') {
-        parent.classList.toggle('hide');
-    }
-    renderStartingBoard();
-}
-
-startGame.addEventListener('click', start);
+  // Render Starting Board
+  // Game Turns
+    // One Player
 
 function updateCell(event) {
-    //const tbody = document.getElementsByTagName('tbody');
-   
-  
     let currentPlayer = gameState.players[0];
     const target = event.target;
     const parent = event.target.parentNode;
-    
     if (target.tagName === "TD" && target.innerText === "") {
       target.innerText = currentPlayer;
       gameState.board[parent.dataset.index][target.dataset.index] = currentPlayer;
-      renderBoard();
       gameState.players.reverse();
-    }
-
-    
-    
-    
-}
-  
-table.addEventListener('click', updateCell);
-
-
-
-//Board Renders
-    // Starting Render
+};
 
 function renderStartingBoard() {
     let tableTemplate = '';
@@ -103,25 +114,14 @@ function renderStartingBoard() {
     table.innerHTML = tableTemplate;
 }
 
-    // Mid-Game Render
 
-function renderBoard() {
-    tbody.remove();
-    let tableTemplate = '';
-    for (let i = 0; i < gameState.board.length; i++) {
-        let row = gameState.board[i];
-        console.log(row);
-        let rowTemplate = `
-            <tr data-index = "${i}">
-                <td data-index = "0">${row[0]}</td>
-                <td data-index = "1">${row[1]}</td>
-                <td data-index = "2">${row[2]}</td>
-            </tr>
-        `
-        tableTemplate += rowTemplate;
-    }
-    table.insertAdjacentHTML("afterbegin", tableTemplate);
+
+    
+    
+    
 }
+  
+table.addEventListener('click', updateCell);
 
 
     
